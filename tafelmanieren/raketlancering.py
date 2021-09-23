8a3097a83bc4cd9e2ccb24c0b47357936d5b19fb
emkse <99069595@mydavinci.nl>
2021-09-23 09:47:54 +0200

    tafelmanieren

---
+++ tafelmanieren/raketlancering.py
@@ -0,0 +1,3 @@
+for a in range (30,0,-1):
+    print(a)
+print("raket is gelanceerd")
---
+++ tafelmanieren/tafelberekenen.py
@@ -0,0 +1,5 @@
+# tafel berekenen
+
+som = int(input("welke tafel wil je weten"))
+for a in range(1,11):
+    print(a,"x", som, "=",a * som)
